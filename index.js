735f127cba8d0dcbe3500132bb44d55792fb50a0
jackzj <ybybzj@gmail.com>
2015-08-10 17:58:00 +0800

    fix plugin loading bug; change option name 'dir' back to 'taskDir'

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins');
+var loadPlugins = require('gulp-load-plugins');
 var $node = {
   path: require('path')
 };
 var extend = require('./lib/extend');
+
 var defaultOptions = {
-  dir: $node.path.join(process.cwd(), 'gulp','tasks')
+  taskDir: $node.path.join(process.cwd(), 'gulp','tasks'),
+  pkgFile: $node.path.join(process.cwd(), 'package.json')
 };
+
 module.exports = function loadTasks(options){
   options = extend(defaultOptions, options);
-  var tasks = require('rqdir')(options.dir);
-  delete options.dir;
+  var tasks = require('rqdir')(options.taskDir);
+  var $ = loadPlugins({
+    config: options.pkgFile,
+  });
   Object.keys(tasks).forEach(function(loaderName){
     tasks[loaderName](gulp, $, options);
   });
 };
-
-// console.log(defaultOptions.taskDir);
