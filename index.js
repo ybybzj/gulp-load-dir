b52e8c8f2ae8eef123f651ebd53c1ee211ee3bc5
jackzj <ybybzj@gmail.com>
2015-08-10 15:59:31 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins');
+var $node = {
+  path: require('path')
+};
+var extend = require('./lib/extend');
+var defaultOptions = {
+  taskDir: $node.path.join(process.cwd(), 'tasks')
+};
+exports.loadTasks = function(options){
+  options = extend(defaultOptions, options);
+  var tasks = require('rqdir')(options.taskDir);
+  delete options.taskDir;
+  Object.keys(tasks).forEach(function(loaderName){
+    tasks[loaderName](gulp, $, options);
+  });
+};
+
+// console.log(defaultOptions.taskDir);
